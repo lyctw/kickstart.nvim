f8bf07c9745ca7e28d9ce96866b9069d332da413
Yu-Chien Lin <mail@yclin.org>
2025-01-24 03:40:05 -0800

    Support Cscope

--- init.lua
+++ init.lua
@@ -451,6 +451,20 @@ require('lazy').setup({
     end,
   },
 
+  -- Cscope
+  {
+    'dhananjaylatkar/cscope_maps.nvim',
+    dependencies = {
+      'nvim-telescope/telescope.nvim', -- optional [for picker="telescope"]
+      'ibhagwan/fzf-lua', -- optional [for picker="fzf-lua"]
+      'echasnovski/mini.pick', -- optional [for picker="mini-pick"]
+    },
+    opts = {
+      -- USE EMPTY FOR DEFAULT OPTIONS
+      -- DEFAULTS ARE LISTED BELOW
+    },
+  },
+
   -- LSP Plugins
   {
     -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
